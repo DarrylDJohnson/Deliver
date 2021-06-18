9350afe372351ef1c132969e9f5d9e9c145d5cef
darryldjohnson <darryl.johnson.w@gmail.com>
2021-06-18 18:25:32 -0500

    Created Project, Added Directories

---
+++ lib/models/inventory.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Inventory{
+
+  final Map<String, int> products;
+
+  const Inventory(this.products);
+}
---
+++ lib/models/product.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Product{
+
+  final String id;
+
+  const Product(this.id);
+}
